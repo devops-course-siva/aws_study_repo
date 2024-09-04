7bedd9735faa8c48c179f32c36a31aff29fb5dd9
siva-nag <99887102+Sivanagu-madepalli@users.noreply.github.com>
2024-09-04 19:06:22 +0530

    updating stop_container

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-#containerid =`docker ps | awk -F" " '{print $1}'`
+containerid =`docker ps | awk -F" " '{print $1}'`
 
-#docker rm -f $containerid 
-
-echo "Hi"
+docker rm -f $containerid 
