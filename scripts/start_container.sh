bdd7abcf21bff8df2a0e9f607410de9bece939db
Sivanagu-madepalli <sivanagurgukt99@gmail.com>
2024-09-05 14:41:07 +0530

    project name change

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-docker pull sivanagu2607/aws-cicd-project:latest
+docker pull sivanagu2607/sample-flask-app:latest
 
-docker run -d -p 5000:5000 sivanagu2607/aws-cicd-project:latest
+docker run -d -p 5000:5000 sivanagu2607/sample-flask-app:latest
 
 echo "Successfully executed start_container.sh"
